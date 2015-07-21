7a15ae136bcb035104618973f33f8de3c7c147b3
DalSoft <d.jones@dalsoft.co.uk>
2015-07-21 12:37:25 +0100

    WebApi bootstrap without any bloat using Owin hosted on IIS.

---
+++ DalSoft.WebApi.Bootstrap.SystemWeb/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace DalSoft.WebApi.SystemWeb.Bootstrap.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet, Route("test/hello")]
+        public HttpResponseMessage Hello()
+        {
+            return new HttpResponseMessage
+            {
+                Content = new StringContent("world"),
+            };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# WebApi-IIS-Bootstrap
-WebApi bootstrap without any bloat using Owin hosted on IIS
+# DalSoft.WebApi.Bootstrap.SystemWeb
+WebApi bootstrap without any bloat using Owin hosted on IIS.
+
+See my blog post http://www.dalsoft.co.uk/blog/index.php/2015/07/21/professional-webapi-part-1-webapi-bootstrap-without-any-bloat-using-owin/
+
+Test it works GET /test/hello 
